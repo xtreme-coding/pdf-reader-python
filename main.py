07fac3a426d0ff4f4181f5a6acd86ff8a3cdb9e4
Soubhagya <nikusoubhagyaranjan@gmail.com>
2020-12-12 18:04:09 +0530

    extracted text and converted to speech

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pyttsx3
+import PyPDF2
 
+book = open('demo.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
 speaker = pyttsx3.init()
-speaker.say('Hey Cortana')
-speaker.runAndWait()
+page = pdfReader.getPage(1)
+text = page.extractText()
+speaker.say(text)
+speaker.runAndWait()   
